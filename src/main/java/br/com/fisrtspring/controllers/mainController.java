9454cbec9b34f3d439b2af6040a9101beaf2378e
Micael Fernandes <micaelnandes21@hotmail.com>
2022-07-30 04:09:23 -0300

    add find by name method

--- src/main/java/br/com/fisrtspring/controllers/mainController.java
+++ src/main/java/br/com/fisrtspring/controllers/mainController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -59,5 +60,33 @@ public class mainController {
         } else {
             return new ResponseEntity<Usuario>(new Usuario(), HttpStatus.FORBIDDEN);
         }
+    }
+
+    @GetMapping(value = "select")
+    @ResponseBody
+    public ResponseEntity<Usuario> selectUser(@RequestParam Long iduser) {
+        Optional<Usuario> selectedUser = usuarioRepository.findById(iduser);
+
+        if (selectedUser.isPresent()) {
+            return new ResponseEntity<Usuario>(selectedUser.get(), HttpStatus.FOUND);
+        } else {
+            return new ResponseEntity<Usuario>(new Usuario(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping(value = "update")
+    @ResponseBody
+    public ResponseEntity<Usuario> update(@RequestBody Usuario upUser) {
+        usuarioRepository.saveAndFlush(upUser);
+        return new ResponseEntity<Usuario>(upUser, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "allusers")
+    @ResponseBody
+    public ResponseEntity<List<Usuario>> userByName(@RequestParam(name = "name") String name) {
+
+        return new ResponseEntity<List<Usuario>>(
+                usuarioRepository.findAllName(name.trim().toLowerCase()),
+                HttpStatus.OK);
     }
 }
--- src/main/java/br/com/fisrtspring/repository/UsuarioRepository.java
+++ src/main/java/br/com/fisrtspring/repository/UsuarioRepository.java
@@ -1,9 +1,13 @@
 package br.com.fisrtspring.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.fisrtspring.models.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
+    @Query(value = "select u from Usuario u where lower(trim(u.nome)) like %?1%")
+    List<Usuario> findAllName(String name);
 }
